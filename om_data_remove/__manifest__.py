db040ecc7cf8b0923bb7994d9fa3682269332c33
jatincreliution <jatinc.reliution@gmail.com>
2025-12-15 12:16:12 +0530

    Bump version from 5.1.2 to 5.1.3

--- om_data_remove/__manifest__.py
+++ om_data_remove/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Odoo 18 Remove Data',
-    'version': '5.1.2',
+    'version': '5.1.3',
     'author': 'Odoo Mates, Sunpop.cn',
     'category': 'Tools',
     'description': 'Data Clean up, Remove Data, Database Clean UP, Reset Database',
